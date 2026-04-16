f6a3340fbadb162d641b5a7b3cef62bbc1f16dc1
MontyPohl <montypohl20@gmail.com>
2026-04-16 11:08:12 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <-- 1. Agrega esta línea
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // <-- 2. Agrega esto aquí
+  ],
 })
